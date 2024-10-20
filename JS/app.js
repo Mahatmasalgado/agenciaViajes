258c7f72bd4c434b95523a49d9d6b460076ad603
Mahatma Salgado <gandhi.salgado.ceron@gmail.com>
2024-10-19 23:25:45 -0600

    Creo que ya terminé todo para los dispositivomos diferentes, solo que no he agregado la funcionalidad del menú expandible

--- JS/app.js
+++ JS/app.js
@@ -1,20 +1,51 @@
 /* Para dispositivos de tablet */
-function tablet(){
-    if (window.screen.width <= 768){
-        const logo = document.querySelectorAll(".encabezado__logo--icono");
-        const navlista = document.querySelector(".encabezado__navegacion--lista");
-        const nav = document.querySelector(".encabezado__navegacion");
+window.addEventListener('resize', function() {
+    // Obtiene el ancho actual de la ventana
+    let width = this.window.innerWidth;
+    console.log(width);
+    
 
-        const menu = document.createElement("img");
-
+    // Aplica diferentes acciones dependiendo del ancho de la ventana
+    if (width <= 430){
+        console.log('Pantalla celular');
+        //cambiamos logo
+        const logo = document.querySelector(".encabezado__logo--icono");
         logo.src = "../img/Logo-pequenio.png";
+    }else if (width <= 768 && width > 430) {
+        console.log('Pantalla de tablet');
+        // obtenemos lista nav
+        const menu = document.querySelector(".encabezado__navegacion--menu");
 
+        if (menu.classList.contains("none")){
+            const navLista = document.querySelectorAll(".encabezado__navegacion--elemento");
+            navLista.forEach(element => {
+                element.classList.add("none");
+            });
 
-        navlista.style.display = "none";
-        menu.src = "../img/menu.png";       // URL de la imagen
-        menu.alt = "Boton menu";
-        nav.appendChild(menu);
+            menu.classList.remove("none");
+        }
+    }else{
+        console.log('Pantalla de escritorio');
+
+        // obtenemos lista nav
+        const navLista = document.querySelectorAll(".encabezado__navegacion--elemento");
+
+        if (navLista[0].classList.contains("none")){
+            //agregamos logo primero
+            const logo = document.querySelector(".encabezado__logo--icono");
+            logo.src = "../img/logoprovisriablanca-1@2x.png";
+
+            //modificamos lo demás
+            navLista.forEach(elemento => {
+                elemento.classList.remove("none");
+            });
+            const menu = document.querySelector(".encabezado__navegacion--menu");
+            menu.classList.add("none")
+        }
+
     }
-}
 
-tablet();
+});
+
+// También puedes ejecutar el código una vez al cargar la página para aplicar las propiedades iniciales
+window.dispatchEvent(new Event('resize'));
